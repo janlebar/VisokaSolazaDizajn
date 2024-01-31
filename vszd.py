fc3f016d1f988eae9500e2b8f9e278dde9b330a5
JAN <janstefanlebar@gmail.com>
2024-01-31 18:39:22 +0100

    update

--- vszd.py
+++ vszd.py
@@ -73,6 +73,10 @@ def convert_ics_to_html(ics_file_path):
         html_output += f"<td>{event[6]}</td>"
         html_output += "</tr>"
 
+    # Add empty rows for sobota and nedelja
+    html_output += "<tr style='background-color: lightgray;'><td></td><td></td><td></td><td></td><td></td></tr>"
+    html_output += "<tr style='background-color: lightgray;'><td></td><td></td><td></td><td></td><td></td></tr>"
+
     html_output += "</table>"
     html_output += "</body></html>"
 
@@ -90,4 +94,34 @@ if __name__ == "__main__":
 
 
 
+input_text = '''
+KONSTRUKCIJA IN MODELIRANJE II Predavanje Damjana Celcar---03:15
+KONSTRUKCIJA IN MODELIRANJE II Predavanje Damjana Celcar---04:00
+OBLIKOVANJE PROSTORA Vaje Petra Kocjančič---04:00
+OSNOVE VEKTORSKE GRAFIKE IN RAČUNALNIŠKE OBDELAVE SLIK (IZREDNI) Predavanje Domen Lo---03:15
+OSNOVE VEKTORSKE GRAFIKE IN RAČUNALNIŠKE OBDELAVE SLIK (IZREDNI) Predavanje Domen Lo---04:00
+OSNOVE VEKTORSKE GRAFIKE IN RAČUNALNIŠKE OBDELAVE SLIK Predavanje Domen Lo---04:00
+OSNOVE VEKTORSKE GRAFIKE IN RAČUNALNIŠKE OBDELAVE SLIK Predavanje Domen Lo---04:00
+'''
 
+
+lines = input_text.strip().split('\n')
+events = {}
+
+for line in lines:
+    event_name, time_str = line.rsplit('---', 1)
+    hours, minutes = map(int, time_str.split(':'))
+
+    if event_name not in events:
+        events[event_name] = {'hours': 0, 'minutes': 0}
+
+    events[event_name]['hours'] += hours
+    events[event_name]['minutes'] += minutes
+
+result = {}
+
+for event_name, time in events.items():
+    total_minutes = time['hours'] * 60 + time['minutes']
+    hours = total_minutes // 60
+    minutes = total_minutes % 60
+    result[event_name] = f'{hours:02}:{minutes:02}'
